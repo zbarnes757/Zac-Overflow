c19dc05309c8aa2d7227ad3d0240a81aae72b002
Zac Barnes <zbarnes@qubiclemonkey.com>
2015-03-20 13:45:39 -0700

    add validations and associations to model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,2 +1,4 @@
 class Answer < ActiveRecord::Base
+	validates :content, presence: true
+	belongs_to :question
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,4 @@
 class Question < ActiveRecord::Base
+	validates :title, :content, presence: true
+	has_many :answers
 end
